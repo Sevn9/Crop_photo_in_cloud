590cf022a681c1f79b2fcfb2c877ea93c70c044e
Artur Shamiyarov <dz3nls7@gmail.com>
2022-12-25 00:23:01 +0300

    added function

---
+++ Crop_photo_in_cloud/vvot30-face-detection.py
@@ -0,0 +1,89 @@
+import requests as req
+import boto3
+import base64
+import os
+import json
+
+URL = 'https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze'
+MQ_URL = 'MQ_URL'
+API_KEY = os.environ['API_SECRET_KEY']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+def get_face_detection_request(content):
+    return {
+        "analyze_specs": [{
+            "content": content,
+            "features": [{
+                "type": "FACE_DETECTION"
+            }]
+        }]
+    }
+
+
+def get_object(bucket, name):
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        endpoint_url='https://storage.yandexcloud.net'
+    )
+    print(f"Getting {name} file from {bucket}")
+    response = s3.get_object(
+        Bucket=bucket,
+        Key=name
+    )
+    print(f"{name} downloaded")
+    return response['Body'].read()
+
+
+def find_faces(img):
+    encoded = base64.b64encode(img).decode('UTF-8')
+    auth_header = {'Authorization': f'Api-Key {API_KEY}'}
+    body = get_face_detection_request(encoded)
+    resp = req.post(URL, json=body, headers=auth_header)
+    coordinates = []
+    try:
+        faces = resp.json()['results'][0]['results'][0]['faceDetection']['faces']
+        for face in faces:
+            coordinates.append(face['boundingBox']['vertices'])
+    except KeyError:
+        print(f'Cant find faces info in {resp.json()}')
+        return []
+    return coordinates
+
+
+def to_message(object_key, face):
+    return {
+        'object_key': object_key,
+        'face': face
+    }
+
+
+def send_to_queue(object_key, faces):
+    session = boto3.session.Session()
+    sqs = session.client(
+        service_name='sqs',
+        endpoint_url='https://message-queue.api.cloud.yandex.net',
+        region_name='ru-central1',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY
+    )
+    messages = [to_message(object_key, face) for face in faces]
+    for message in messages:
+        body = json.dumps(message)
+        print(f'Trying to send {body}')
+        sqs.send_message(
+            QueueUrl=MQ_URL,
+            MessageBody=body,
+            MessageDeduplicationId=object_key
+        )
+
+
+def handler(event, context):
+    bucket = event['messages'][0]['details']['bucket_id']
+    name = event['messages'][0]['details']['object_id']
+    img = get_object(bucket, name)
+    send_to_queue(name, find_faces(img))
