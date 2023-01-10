6c9e752dda54e0e7ab1e2da6fd2d239e1d03d726
Sevn9 <72407422+Sevn9@users.noreply.github.com>
2023-01-11 00:39:47 +0300

    fixed bot and method /getface

--- Crop_photo_in_cloud/vvot30-boot.py
+++ Crop_photo_in_cloud/vvot30-boot.py
@@ -1,27 +1,27 @@
-import telegram
 import os
 import ydb
 import ydb.iam
 import json
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
-BOT = telegram.Bot(token=TOKEN)
+
 driver: ydb.Driver
 PHOTO_LINK_TEMPLATE = os.getenv("PHOTO_LINK_TEMPLATE")
 OBJECT_LINK_TEMPLATE = os.getenv("OBJECT_LINK_TEMPLATE")
 
-
-def get_driver():
+def initDb():
     endpoint = os.getenv("DB_ENDPOINT")
     path = os.getenv("DB_PATH")
     creds = ydb.iam.MetadataUrlCredentials()
+
     driver_config = ydb.DriverConfig(
         endpoint, path, credentials=creds
     )
+
     return ydb.Driver(driver_config)
 
-
-def get_face(chat_id):
+def getFace(chat_id):
     query = f"""
     PRAGMA TablePathPrefix("{os.getenv("DB_PATH")}");
     SELECT * FROM photo WHERE name is NULL LIMIT 1;
@@ -29,31 +29,32 @@ def get_face(chat_id):
     session = driver.table_client.session().create()
     result_sets = session.transaction().execute(query, commit_tx=True)
     session.closing()
+
     for row in result_sets[0].rows:
-        face_id = row.face_id
-        photo_url = PHOTO_LINK_TEMPLATE.format(face_id)
-        BOT.send_photo(chat_id=chat_id, photo=photo_url)
+        face_id = row.face_id.decode("utf-8")
+        photo_url = PHOTO_LINK_TEMPLATE + face_id
+        sendPhoto(chat_id, photo_url)
 
-
-def add_name_to_last_photo(name):
+def setNameToPhoto(name):
     query = f"""
         PRAGMA TablePathPrefix("{os.getenv("DB_PATH")}");
         SELECT * FROM photo WHERE name is NULL LIMIT 1;
         """
     session = driver.table_client.session().create()
     result_sets = session.transaction().execute(query, commit_tx=True)
+
     face_id = ''
     for row in result_sets[0].rows:
-        face_id = row.face_id
+        face_id = row.face_id.decode("utf-8")
     if face_id == '':
         return
+
     query = f"""
     PRAGMA TablePathPrefix("{os.getenv("DB_PATH")}");
     UPDATE photo SET name = '{name}' WHERE face_id = '{face_id}';
     """
     session.transaction().execute(query, commit_tx=True)
     session.closing()
-
 
 def find(chat_id, name):
     query = f"""
@@ -63,37 +64,47 @@ def find(chat_id, name):
     session = driver.table_client.session().create()
     result_sets = session.transaction().execute(query, commit_tx=True)
     session.closing()
+
     if len(result_sets[0].rows) == 0:
-        BOT.sendMessage(chat_id, text=f'No photos with {name}')
+        sendMessage(chat_id, f'Нет фотографии с именем {name}')
     for row in result_sets[0].rows:
-        object_id = row.original_id
-        photo_url = OBJECT_LINK_TEMPLATE.format(object_id)
-        BOT.send_photo(chat_id=chat_id, photo=photo_url)
+        object_id = row.original_id.decode("utf-8")
+        photo_url = OBJECT_LINK_TEMPLATE + object_id
+        sendPhoto(chat_id, photo_url)
 
-
-def set_up():
+def init():
     global driver
-    driver = get_driver()
+    driver = initDb()
     driver.wait(timeout=5)
 
-
 def handler(event, context):
-    set_up()
-    request = event['body']
-    update = telegram.Update.de_json(json.loads(request), BOT)
-
-    chat_id = update.message.chat.id
-    command = update.message.text.encode('utf-8').decode()
+    init()
+    
+    body = json.loads(event['body'])
+    chat_id = body['message']['from']['id']
+    command = body['message']['text']
 
     if command == '/start':
-        BOT.sendMessage(chat_id=chat_id, text='Hello)')
+        sendMessage(chat_id, 'Bot started')
         return
     if command == '/getface':
-        get_face(chat_id)
+        getFace(chat_id)
         return
     if command.startswith('/find'):
         args = command.split(' ')
         find(chat_id, args[1])
         return
-    add_name_to_last_photo(command)
-    BOT.sendMessage(chat_id=chat_id, text=f'Added new name {command}')
+
+    setNameToPhoto(command)
+    sendMessage(chat_id, f'Добавлено новое имя {command}')
+
+def sendMessage(chat_id, text):
+    url = 'https://api.telegram.org/bot' + TOKEN + '/' + 'sendMessage'
+    data = {'chat_id': chat_id, 'text': text}
+    r = requests.post(url, data=data)
+
+def sendPhoto(chat_id, photo):
+    url = 'https://api.telegram.org/bot' + TOKEN + '/' + 'sendPhoto'
+    data = {'chat_id': chat_id, 'photo': photo}
+    r = requests.post(url, data=data)
+    print(r)
